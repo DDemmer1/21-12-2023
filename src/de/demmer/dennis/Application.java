7999e2a7ac50992499d5f4b842998aa9b0763062
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-12-21 17:52:30 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,8 +1,16 @@
 package de.demmer.dennis;
 
+
 public class Application {
 	
 	public static void main(String[] args) {
+
+		
+		User user1 = new User("Dennis", "12345");
+		User user2 = new User("Nils", "password");
+		
+		int[] iArr = new int[150];
+
 		
 	}
 
---
+++ src/de/demmer/dennis/User.java
@@ -0,0 +1,21 @@
+package de.demmer.dennis;
+
+public class User {
+	
+	
+	String name;
+	String pw;
+	String[] nicknames;
+	
+	
+	public User(String name, String pw) {
+		this.name = name;
+		this.pw = pw;
+		nicknames = new String[150];
+	}
+	
+	
+	
+	
+
+}
